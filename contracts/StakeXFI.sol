247cd79c9e187bd6021837b642293593a6295ed6
sorleuCode <muhammedsoliu2222@gmail.com>
2024-10-17 10:47:23 +0100

    update

--- contracts/StakeXFI.sol
+++ contracts/StakeXFI.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.27;
 
 import "./IERC20.sol";
 
-contract StakeXFI {
+contract StakeXFI{
     IERC20 public XFIContract;
     IERC20 public MPXContract;
 
     address internal owner;
     address internal newOwner;
 
-    bool locked;
+    bool internal locked;
 
-    uint256 immutable MINIMUM_STAKE_AMOUNT = 1_000 * (10**18);
-    uint256 immutable MAXIMUM_STAKE_AMOUNT = 100_000 * (10**18);
-    
+    uint256 immutable MINIMUM_STAKE_AMOUNT = 1000 * (10**18);
+    uint256 immutable MAXIMUM_STAKE_AMOUNT = 100000 * (10**18);
 
-    struct Staking {
+    uint32 internal constant REWARD_PER_SECOND = 1000000; // 0.000001% 10e11
+
+    struct Staking{
         uint256 amount;
         uint256 startTime;
         uint256 duration;
@@ -25,39 +26,34 @@ contract StakeXFI {
 
     mapping (address => Staking[]) stakers;
 
-
-    constructor(address _xfiAddress, address _mpxAddress) {
+    constructor(address _xfiAddress, address _mpxAddress){
         XFIContract = IERC20(_xfiAddress);
         MPXContract = IERC20(_mpxAddress);
 
         owner = msg.sender;
-
     }
 
-//events
-    event DepositSuccessful(address indexed staker, uint256 amount, uint256 indexed startTime);
+    event DepositSuccessful(address indexed  staker, uint256 amount, uint256 indexed startTime);
     event WithdrawalSuccessful(address indexed staker, uint256 amount, uint256 indexed reward);
-    event OwnershipTransfered(address indexed previousOwner, address newOwner);
+    event OwnershipTransfered(address indexed previousOwner, address indexed newOwner);
 
-//modifiers
+    modifier onlyOwner(){
+        require(msg.sender == owner, "No Access");
+        _;
+    }
+
     modifier reentrancyGuard() {
         require(!locked, "Not allowed to re-enter");
-
         locked = true;
         _;
         locked = false;
     }
-    modifier onlyOwner(){
-        require(msg.sender == owner, "Only owner can access");
-        _;
-    }
 
-
-    function stake (uint256 _amount, uint256 _duration) external  reentrancyGuard {
+    function stake(uint256 _amount, uint256 _duration) external reentrancyGuard {
         require(msg.sender != address(0), "Zero address not allowed");
 
         require(_amount >= MINIMUM_STAKE_AMOUNT && _amount <= MAXIMUM_STAKE_AMOUNT, "Amount is out of range");
-        
+
         require(_duration > 0, "Duration is too short");
 
         require(XFIContract.balanceOf(msg.sender) >= _amount, "You don't have enough");
@@ -67,33 +63,32 @@ contract StakeXFI {
         XFIContract.transferFrom(msg.sender, address(this), _amount);
 
         Staking memory staking;
-
         staking.amount = _amount;
         staking.duration = block.timestamp + _duration;
         staking.startTime = block.timestamp;
 
         stakers[msg.sender].push(staking);
-    }
+
+        emit DepositSuccessful(msg.sender, _amount, block.timestamp);
+    } 
 
 
-    function withdrawStake (uint256 _index) external  reentrancyGuard returns(bool) {
-        require(msg.sender != address(0), "Zero not allowed");
-
+    function withdrawStake(uint8 _index) external reentrancyGuard returns (bool) {
+        require(msg.sender != address(0), "Zero address not allowed");
         require(_index < stakers[msg.sender].length, "Out of range");
 
         Staking storage staking = stakers[msg.sender][_index];
 
         require(block.timestamp > staking.duration, "Not yet time");
+        require(!staking.hasWithdrawn, "Stake already withdrawn");
 
         uint256 amountStaked_ = staking.amount;
-        uint256 rewardAmount_ = calculateReward(block.timestamp, staking.duration, staking.amount);
-
+        uint256 rewardAmount_ = calculateReward(staking.startTime, staking.duration);
 
         staking.hasWithdrawn = true;
         staking.amount = 0;
         staking.startTime = 0;
         staking.duration = 0;
-
 
         XFIContract.transfer(msg.sender, amountStaked_);
         MPXContract.transfer(msg.sender, rewardAmount_);
@@ -104,35 +99,34 @@ contract StakeXFI {
     }
 
 
-    function getStakerInfo (uint8 _index) external view returns (Staking memory) {
-         require(msg.sender != address(0), "Zero not allowed");
+    function getStakerInfo(uint8 _index) external view returns (Staking memory){
+        require(msg.sender != address(0), "Zero address not allowed");
 
         require(_index < stakers[msg.sender].length, "Out of range");
 
-        Staking storage staking = stakers[msg.sender][_index];
-
+        Staking memory staking = stakers[msg.sender][_index];
         return staking;
     }
 
-    function getContractXFIBalance () external onlyOwner view returns (uint256) {
-        uint256 bal = XFIContract.balanceOf(address(this));
-
-        return bal;
-    }
-
-    function getContractMPXBalance () external onlyOwner view returns (uint256) {
+    function getContractMPXBalance() external onlyOwner view returns (uint256){
         uint256 bal = MPXContract.balanceOf(address(this));
-
         return bal;
     }
 
-    function TransferOwnership(address _newOwner) external onlyOwner {
-        require(_newOwner != address(0), "Zero not allowed" );
+    function getContractXFIBalance() external onlyOwner view returns (uint256){
+        uint256 bal = XFIContract.balanceOf(address(this));
+        return bal;
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner{
+        require(_newOwner != address(0), "Zero address not allowed");
+
         newOwner = _newOwner;
     }
-    function claimOwnership() external onlyOwner {
-        require(msg.sender != address(0), "Zero not allowed" );
-        require (msg.sender == newOwner, "Not your turn");
+
+    function claimOwnership() external {
+        require(msg.sender != address(0), "Zero address not allowed");
+        require(msg.sender == newOwner, "Not your turn");
 
         emit OwnershipTransfered(owner, newOwner);
 
@@ -140,13 +134,9 @@ contract StakeXFI {
         newOwner = address(0);
     }
 
-    function calculateReward(uint256 _startTime, uint256 _endTime, uint256 _amount ) private pure returns(uint256) {
-        uint256 stakeDuration = _endTime - _startTime;
-
-        uint256 totalReward = (1000000 * _amount * stakeDuration) * 1e7;
-        return totalReward;
-
+    function calculateReward(uint256 _startTime, uint256 _endTime) private pure returns (uint256){
+    uint256 stakeDuration = _endTime - _startTime;
+    return stakeDuration * REWARD_PER_SECOND;
     }
-
 
 }
